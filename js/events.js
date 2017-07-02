d086bd2cb176d82db5128a9af4c71c6f964f87e6
github username <github email address>
2017-07-02 18:47:22 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,40 @@
-//define functions here
+// function getIt(){
+// $('p').on('click', function() {
+//   alert( "hey!" );
+// });
+// }
+function getIt(){
+$('p').click(function() {
+  alert( "hey!" );
+});
+}
+
+function frameIt(){
+  $('img').load(function() {
+    $('img').addClass('tasty')
+  });
+}
+
+function pressIt() {
+  $('#typing').keydown(function(e) {
+    if (e.which === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').submit(function(e) {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
 
 $(document).ready(function(){
 
-// call functions here
+getIt()
+frameIt()
+pressIt()
+submitIt()
 
 });
